1b5ec51197d74c11d5ee86f4cad20414612383a9
MamBoota <deniskostyuk.93@yandex.ru>
2026-04-21 21:24:58 +0300

    Work?!

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -21,14 +21,8 @@ module.exports = {
     host: process.env.DATABASE_HOST,
   },
   test: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME || 'postgres',
-    username: process.env.DATABASE_USERNAME || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'password',
-    port: process.env.DATABASE_PORT || 5432,
-    // 🔥 По умолчанию localhost (для CI и локального запуска без docker-compose)
-    // 🔥 В docker-compose переопределяется через DATABASE_HOST=db
-    host: process.env.DATABASE_HOST || 'localhost',
+    dialect: 'sqlite',
+    storage: ':memory:',
     logging: false,
     pool: {
       max: 5,
